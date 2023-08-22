c0c49631fc63949b2bbb346dacbf81eb7437e7ff
Somil Guha <guhasomil2907@gmail.com>
2023-08-22 23:13:08 +0530

    first push

--- src/apiKeys.js
+++ src/apiKeys.js
@@ -1,5 +1,5 @@
 // Visit https://api.openweathermap.org & then signup to get our API keys for free
 module.exports = {
-  key: "{Your API Key Here}",
+  key: "f0edc6e2be98aa1f3bf27332742c096c",
   base: "https://api.openweathermap.org/data/2.5/",
 };
